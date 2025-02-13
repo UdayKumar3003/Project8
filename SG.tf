db408631c8daff500629c3940131e58b9e63e3b8
kumaru <kumaru@gamil>
2025-02-13 08:32:44 +0530

    this comit is for SG-changes

--- SG.tf
+++ SG.tf
@@ -4,13 +4,13 @@ resource "aws_security_group" "SG" {
 
   ingress {
     from_port = 0
-    to_port = 0
+    to_port = 65000
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port = 0
-    to_port = 0
+    to_port = 60000
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
